7acd16e6adef333b3a55633f9cd0be0520add129
franklin-raph <igboekwulusifranklin@gmail.com>
2023-02-01 16:45:24 +0100

    bug fix

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const dbConnectionString =
-  "mongodb+srv://franklin:emmaofmcap@elx-academy-students.9jg0zcr.mongodb.net/?retryWrites=true&w=majority";
+const dbConnectionString = process.env.MONGO_REMOTE_URI;
 console.log(dbConnectionString);
 mongoose.set("strictQuery", true);
 
@@ -17,5 +16,3 @@ const dbConnectinMethod = async () => {
 };
 
 module.exports = { dbConnectinMethod };
-
-// process.env.MONGO_REMOTE_URI;
