88617dafab2e8c38526602532e9a33002c0577e7
franklin-raph <igboekwulusifranklin@gmail.com>
2023-02-01 16:23:15 +0100

    added a home page for the servet

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const db = require('./config/connectDb')
-require('dotenv').config();
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require("express");
+const db = require("./config/connectDb");
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // middlewares
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-app.use('/api/v1/', require('./routes/studentRoutes'))
-app.use('/api/v1/', require('./routes/adminRoutes'))
+app.use("/api/v1/", require("./routes/studentRoutes"));
+app.use("/api/v1/", require("./routes/adminRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.listen(PORT, () => {
-    db.dbConnectinMethod()
-    console.log(`Server started on port ${PORT}`)
-})
+  db.dbConnectinMethod();
+  console.log(`Server started on port ${PORT}`);
+});
