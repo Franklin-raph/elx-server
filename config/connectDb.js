81d9dbd385776f52b97c1203ed0fa9d17d324d2a
franklin-raph <igboekwulusifranklin@gmail.com>
2023-02-01 17:37:38 +0100

    changed my connecting string

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const dbConnectionString = process.env.MONGO_REMOTE_URI;
+const dbConnectionString =
+  "mongodb+srv://franklin:emmaofmcap@elx-academy-students.9jg0zcr.mongodb.net/?retryWrites=true&w=majority";
 console.log(dbConnectionString);
 mongoose.set("strictQuery", true);
 
@@ -16,3 +17,5 @@ const dbConnectinMethod = async () => {
 };
 
 module.exports = { dbConnectinMethod };
+
+// process.env.MONGO_REMOTE_URI;
